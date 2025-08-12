1ca109dcf1b95b14b26e449bcc86b2e6196c6fe3
Gauribnair <156586648+Gauribnair@users.noreply.github.com>
2025-08-12 12:07:30 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#5D4265',
+        secondary: '#7C647D',
+        accent: '#B09DAC',
+        light: '#F7F2F5',
+        darkText: '#886194',
+        dark: '#3A293D'
+      }
+    }
+  },
+  plugins: []
+}
